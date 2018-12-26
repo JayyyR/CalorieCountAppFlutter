b2facb59f6cda533988b41bd8e2d8e855d042888
Joe Acosta <jacosta@okcupid.com>
2018-12-25 22:13:01 -0500

    viewmodel seperation of concerns

--- lib/AddCaloriesPage.dart
+++ lib/AddCaloriesPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
 class AddCaloriesPage extends StatefulWidget {
   final Function daySavedCallback;
@@ -10,9 +11,40 @@ class AddCaloriesPage extends StatefulWidget {
 }
 
 class _AddCaloriesPageState extends State<AddCaloriesPage> {
+
+  _AddCaloriesPageViewModel viewModel;
+
+  _AddCaloriesPageState() {
+    //initialize viewmodel with trigger to update state
+    viewModel = _AddCaloriesPageViewModel(triggerViewStateChange: (){
+      setState(() {
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return null;
   }
+}
+
+class _AddCaloriesPageViewModel {
+
+  final VoidCallback triggerViewStateChange;
+  final PublishSubject<int> dayFinished = PublishSubject();
+
+  _AddCaloriesPageViewModel({@required this.triggerViewStateChange});
+
+  int totalCaloriesForDay = 0;
+
+  void onAddCaloriesToDayPressed(int caloriesInMeal) {
+    //add calories to total
+    totalCaloriesForDay += caloriesInMeal;
+
+    //trigger view state
+    triggerViewStateChange();
+  }
+
+  void dispose() => dayFinished.close();
 }
